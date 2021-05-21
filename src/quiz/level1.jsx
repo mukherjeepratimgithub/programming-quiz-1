c58a1bb73f066022db897506adf3473303ca5588
Pratim Mukherjee <49569038+mukherjeepratimgithub@users.noreply.github.com>
2021-05-21 17:10:24 +0530

    Initial commit

---
+++ src/quiz/level1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const burgers = (ref) => (
+  <div className="burgers" ref={ref}>
+    <div className="burger for-mr-johnson">
+      <div className="burger bottom-bun-onion-beef-cheese-pickles-egg-top-bun "></div>
+    </div>
+    <div className="burger for-mrs-johnson">
+      <div className="burger bottom-bun-tomato-beef-cheese-pepper-lettuce-top-bun "></div>
+    </div>
+    <div className="burger for-little-peter">
+      <div className="burger bottom-bun-tomato-bacon-cheese-egg-top-bun "></div>
+    </div>
+  </div>
+);
